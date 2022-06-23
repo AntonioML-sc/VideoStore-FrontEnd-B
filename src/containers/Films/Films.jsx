1e714f99d887014c1801d8e0ece10e370529c089
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-06-23 23:20:34 +0200

    Films initial design

---
+++ src/containers/Films/Films.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Films.css"
+
+const Films = props => {
+
+
+    return (
+        <div className="cont">
+            <div className="searchBar">SearchBar</div>
+            <div className="filmsList">
+                Container for film cards
+            </div>
+        </div>
+    )
+}
+
+export default Films
