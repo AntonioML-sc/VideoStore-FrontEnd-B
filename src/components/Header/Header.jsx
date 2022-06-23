2f8ab54642141d753cef01b42e40fd0816bae896
jackson <jackson55adl@gmail.com>
2022-06-23 18:00:34 +0200

    header done, still need to finalize some details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import "./Header.css"
 
 const Header = () => {
-    
+    return (
+        <div id="Headerwall">
+
+            <div className="headeritems">
+            <a className="headerlinks" href="http://"><img src="" alt="LOGO" /></a>
+            </div>
+            <div className="headeritems">
+                <div className="item"><a className="headerlinks" href="http://">Home</a></div>
+                <div className="item"><a className="headerlinks" href="http://">Films</a></div>
+                <div className="item"><a className="headerlinks" href="http://">About us</a></div>
+            </div>
+            <div className="headeritems">
+                <div className="item"><a className="headerlinks" href="http://">Sign up</a></div>
+                <div className="item"><a className="headerlinks" href="http://">Log in</a></div>
+            </div>
+
+        </div>
+    )
 }
+
+export default Header;
