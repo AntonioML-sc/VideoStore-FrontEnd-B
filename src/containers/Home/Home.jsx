201e0a81f9dc3d08a1fda1390ec726c211e9d249
jackson <jackson55adl@gmail.com>
2022-06-23 18:49:53 +0200

    Home structuring

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+import "./Home.css"
+
+
+const Home = (props) =>{
+
+    return(
+        <div id="Homewall">
+            <div>
+                <p>"Literally, on fire..."</p>
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+
+}
+
+export default Home;
+
