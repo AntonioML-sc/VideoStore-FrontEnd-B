be964f31218b0edad21ec59bc23f7732979bb5d9
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-06-23 23:34:53 +0200

    Add Films container route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header"
 import Footer from './components/Footer/Footer'
 import AboutUs from './containers/AboutUs/AboutUs'
 import Home from "./containers/Home/Home";
+import Films from "./containers/Films/Films";
 import './App.css'
 
 
@@ -16,14 +17,12 @@ function App() {
       <BrowserRouter>
       <Header />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-
+          <Route path="/" element={<Home/>}/>          
           <Route path="/aboutus" element={<AboutUs />}/>
-        
+          <Route path="/films" element={<Films />}/>        
         </Routes>
         <Footer />
       </BrowserRouter>
-
     </div>
   )
 }
