d29db1b94a5f3d261cb08aeb52b9ab1cadbb22eb
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-06-25 19:53:00 +0200

    Add form to search film by title

--- src/containers/Films/Films.jsx
+++ src/containers/Films/Films.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import "./Films.css"
 import axios from 'axios'
 
@@ -10,19 +10,44 @@ const Films = props => {
     })
 
     useEffect(() => {
-        axios.get('http://localhost:5000/films')
-        .then(resp => {
-            console.log(resp.data);
-            setData({
-                ...data,
-                films: resp.data
-            });
+        if (data.search == "") {
+            axios.get('http://localhost:5000/films')
+                .then(resp => {
+                    console.log(resp.data);
+                    setData({
+                        ...data,
+                        films: resp.data
+                    });
+                })
+        } else {
+            axios.get(`http://localhost:5000/films/getByTitle/${data.search}`)
+                .then(resp => {
+                    console.log(resp.data);
+                    setData({
+                        ...data,
+                        films: resp.data
+                    });
+                })
+        }
+    }, [data.search])
+
+    const handleChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
         })
-    }, [])
+    }
 
     return (
         <div className="cont">
-            <div className="searchBar">SearchBar</div>
+            <div className="searchBar">
+                <form className="searchBarForm">
+                    <div>
+                        <label>Search by title:</label>
+                        <input type="text" value={data.search} onChange={handleChange} name="search" />
+                    </div>
+                </form>
+            </div>
             <div className="filmsList">
                 Container for film cards
             </div>
