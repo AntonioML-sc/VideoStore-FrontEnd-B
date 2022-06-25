099d25c4c0e19e3a38da560fae740db10b8ca445
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-06-25 17:50:27 +0200

    Conect to local database and take films

--- src/containers/Films/Films.jsx
+++ src/containers/Films/Films.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React from "react"
+import {useState, useEffect} from 'react'
 import "./Films.css"
+import axios from 'axios'
 
 const Films = props => {
+    let [films, setFilms] = useState([])
 
+    useEffect(() => {
+        axios.get('http://localhost:5000/films')
+        .then(resp => {
+            console.log(resp.data);
+            setFilms(resp.data);
+        })
+    }, [])
 
     return (
         <div className="cont">
