2d7d1eecd52914f7e665fa83a9955cbb6f358681
jcarlos2n <eljcarlos2n@gmail.com>
2022-06-30 19:31:10 +0200

    application login function

--- src/containers/User/Login/Login.jsx
+++ src/containers/User/Login/Login.jsx
@@ -10,20 +10,59 @@ const Login = props => {
    const [credentials, setCrendentials] = useState({email:'',password:''});
    const [msgError, setMsgError] = useState("");
 
+    let navigate = useNavigate();
 
-    // const handleChange = (event) => {
-    //     setDatos({
-    //         ...datos,
-    //         [event.target.name]: event.target.value
-    //     })
-    // }
+    const dispatch = useDispatch();
+    const credenciales = useSelector(userData);
+
+    const updateCredentials = (event) => {
+        setCrendentials({...credentials, [event.target.name]: event.target.value})
+    }
+
+
+    useEffect(() => {
+        if (credenciales?.token !== '') {
+            navigate('/');
+        };
+    },[]);
+
+    const nosLogea = () => {
+        if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(credentials.email) ) {
+            setMsgError('Introduce un e-mail válido');
+            return;
+        }
+
+        if (credentials.password.length > 4) {
+            if (! /[\d()+-]/g.test(credentials.password)) {
+
+                setMsgError('Password incorrecto');
+                return;
+            }
+        }else{
+            setMsgError('El password debe tener mas de 4 caracteres');
+            return;
+        }
+
+        setMsgError('');
+        dispatch(loginUser({
+            email: credentials.email,
+            password: credentials.password
+        }));
+
+        setTimeout(() => {
+            navigate("/")
+        },1000)
+
+    };
+    
     return (
         <div className="cont">
+             <pre>{JSON.stringify(credentials, null,2)}</pre>
                 <label className="tittle">Email:</label>
-                <input type="text"  name="email" />
+                <input type="email" name="email" onChange={updateCredentials}  />
                 <label className="tittle">Password:</label>
-                <input type="password"   name="pass" />
-                <input type="submit" value="Send" />
+                <input type="password"   name="password" onChange={updateCredentials} />
+                <input type="submit" value="Login" onClick={() => nosLogea()}  />
         </div>
     )
 }
---
+++ src/containers/User/userSlice.js
@@ -0,0 +1,49 @@
+
+
+import { createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
+import jwt from 'jwt-decode';
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+        token: ''
+    },
+    reducers: {
+        login: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            }
+        },
+        logout: (state, action) => {
+            return{
+                ...state.initialState
+            }
+        }
+    }
+});
+
+export const loginUser = (body) => async (dispatch) => {
+    try {
+        const user = await axios.post("https://aml-mysql-28-06-22-videostore.herokuapp.com/users/login", body);
+    
+        let decode = jwt(user.data.token);
+
+        if(user.status === 200) {
+            dispatch(login({...decode,token: user.data.token}))
+        }
+
+    }catch(error){
+        console.log(error)
+    }
+};
+
+export const { login, logout } = userSlice.actions;
+
+export const userData = (state) => state.user;
+
+export default userSlice.reducer;
+
+
+
