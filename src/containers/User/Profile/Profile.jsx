a114e192db8dabf0f4746c3055e6f5f16ef9a5b5
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-09 19:40:30 +0200

    Debug, clean and coment code.

--- src/containers/User/Profile/Profile.jsx
+++ src/containers/User/Profile/Profile.jsx
@@ -9,168 +9,143 @@ import "./Profile.scss"
 import UserCard from "../../../components/UserCard/UserCard";
 import OrderCard from "../../../components/OrderCard/OrderCard";
 
-const Profile = props => {
-    let [data, setData] = useState({
-        users: [],
-        orders: []
-    });
+const Profile = () => {
+
+    let [users, setUsers] = useState([]);
+    let [orders, setOrders] = useState([]);
 
     const dispatch = useDispatch();
     const datos = useSelector(userData);
     let navigate = useNavigate();
 
+    // Log out and go to home page
     const getout = () => {
         dispatch(logout());
         navigate("/")
     }
 
     useEffect(() => {
+
+        const config = {
+            headers: { "Authorization": `Bearer ${datos.token}` }
+        }
+
+        // get a list of all users from db. Visible only if logged user is admin.
         async function fetchUsers() {
-            const config = {
-                headers: { "Authorization": `Bearer ${datos.token}` }
-            }
             await axios.get('https://aml-mysql-28-06-22-videostore.herokuapp.com/users/', config)
                 .then(resp => {
-
-                    setData({
-                        ...data,
-                        users: resp.data
-                    })
-
-                }).catch((error) => {
-                    console.log(error)
-                })
+                    setUsers(resp.data);
+                });
         }
 
+        // get a list of all of the logged user's orders registered in db.
         async function fetchOrders() {
-            const config = {
-                headers: { "Authorization": `Bearer ${datos.token}` }
-            }
             await axios.get('https://aml-mysql-28-06-22-videostore.herokuapp.com/orders/userOrders', config)
                 .then(resp => {
-
-                    setData({
-                        ...data,
-                        orders: resp.data
-                    })
-
-                }).catch((error) => {
-                    console.log(error)
-                })
-            console.log(data.orders)
+                    setOrders(resp.data)
+                });
         }
 
-        fetchUsers()
-        fetchOrders()
+        fetchOrders();
+        fetchUsers();
     }, [])
 
+    // renders the list of users if admin is logged
     const UserList = () => {
-        if (data.users.length > 0) {
-            return (
-                data.users.map((users, index) => (
-                    <div key={index}>
-                        <UserCard data={users} />
+        if (datos.user.role == "admin") {
+            if (users.length > 0) {
+                return (
+                    <div className="ElemList">
+                        <div className="elemTitle">
+                            <br></br>
+                            <h3>Users Registered</h3>
+                            <br></br>
+                        </div>
+                        <div className="list">
+                            {
+                                users.map((user, index) => (
+                                    <UserCard key={index} data={user} />
+                                ))
+                            }
+                        </div>
                     </div>
-                ))
-            )
+                )
+            } else {
+                return (
+                    <div className="elemTitle">
+                        <br></br>
+                        <h3>No Users Registered. We are broke!</h3>
+                        <br></br>
+                    </div>
+                )
+            }
         }
     }
 
+    // renders the list of orders
     const OrderList = () => {
-        if (data.orders.length > 0) {
+        if (orders.length > 0) {
             return (
-                data.orders.map((order, index) => (
-                    <div key={index}>
-                        <OrderCard data={order} />
+                <div className="ElemList" >
+                    <div className="elemTitle">
+                        <h3>Orders Registered</h3>
+                        <br></br>
                     </div>
-                ))
+                    <div className="list">
+                        {
+                            orders.map((order, index) => (
+                                <OrderCard key={index} data={order} />
+                            ))
+                        }
+                    </div>
+                </div>
             )
-        }
-    }
-
-    if (datos.user.role == "admin") {
-        return (
-            <div className="profileWall">
-                <div id="profileCard">
-                    <div id="profilePhoto">
-
-                    </div>
-
-                    <div id="profileInfo">
-                        <div className="profileItem">
-                            <h3 id="profileItemName">{datos.user.name}</h3>
-                        </div>
-                        <div className="profileItem">
-                            <h3>{datos.user.email}</h3>
-                        </div>
-                        <div className="profileItem">
-                            <h3>{datos.user.address}</h3>
-                        </div>
-                        <div className="profileItem">
-                            <h3>{datos.user.phone}</h3>
-                        </div>
-                        <br></br>
-                        <div className="profileItem">
-                            <h3>Date of creation: {datos.user.createdAt}</h3>
-                        </div>
-
-                    </div>
-                </div>
-
-                <button id="logoutButton" onClick={getout}>
-                    Log out
-                </button>
-                <div className="list">
-                    <UserList />
-                </div>
-
-            </div>
-        )
-    } else {
-        return (
-            <div className="profileWall">
-
-                <div id="profileCard">
-                    <div id="profilePhoto">
-
-                    </div>
-
-                    <div id="profileInfo">
-                        <div className="profileItem">
-                            <h3 id="profileItemName">{datos.user.name}</h3>
-                        </div>
-                        <div className="profileItem">
-                            <h3>{datos.user.email}</h3>
-                        </div>
-                        <div className="profileItem">
-                            <h3>{datos.user.address}</h3>
-                        </div>
-                        <div className="profileItem">
-                            <h3>{datos.user.phone}</h3>
-                        </div>
-                        <br></br>
-                        <div className="profileItem">
-                            <h3>Date of creation: {datos.user.createdAt}</h3>
-                        </div>
-
-                    </div>
-                </div>
-
-                <button id="logoutButton" onClick={getout}>
-                    Log out
-                </button>
-
-                <div className="profileItem">
-                    <h3>Orders Registered</h3>
+        } else {
+            return (
+                <div className="elemTitle">
+                    <h3>You do not have orders registered. Look at our catalog and enjoy! </h3>
                     <br></br>
                 </div>
-                <div className="list">
-                    <OrderList />
-                </div>
-
-            </div>
-        )
+            )
+        }
     }
+
+    // renders the container
+    return (
+        <div className="profileWall">
+            <div id="profileCard">
+
+                <div id="profilePhoto"></div>
+
+                <div id="profileInfo">
+                    <div className="profileItem">
+                        <h3 id="profileItemName">{datos.user.name}</h3>
+                    </div>
+                    <div className="profileItem">
+                        <h3>{datos.user.email}</h3>
+                    </div>
+                    <div className="profileItem">
+                        <h3>{datos.user.address}</h3>
+                    </div>
+                    <div className="profileItem">
+                        <h3>{datos.user.phone}</h3>
+                    </div>
+                    <br></br>
+                    <div className="profileItem">
+                        <h3>Date of creation: {datos.user.createdAt}</h3>
+                    </div>
+                </div>
+            </div>
+
+            <button id="logoutButton" onClick={getout}>
+                Log out
+            </button>
+
+            <OrderList />
+
+            <UserList />
+        </div>
+    )
 }
 
 export default Profile
