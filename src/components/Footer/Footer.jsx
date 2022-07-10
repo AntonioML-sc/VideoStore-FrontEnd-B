d03fa19d1f2c53ad0e885db0a44df5e671c5eb3a
“AntonioML-sc” <antoniomoralesledesma@gmail.com>
2022-07-10 22:14:16 +0200

    Clean code in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,31 +2,9 @@ import React from "react";
 import './Footer.scss'
 import { ModalFooter } from "react-bootstrap";
 
-const Footer = props => {
-
+const Footer = () => {
 
     return(
-        // <ModalFooter className="footer">
-        //     <div className="footLeft">
-        //          <a href="">Film News</a><br />
-        //          <a href="">Careers</a><br />
-        //          <a href="">Press Lounge</a><br />
-        //      </div>
-        //      <div>
-        //          <div className="logoContainer">
-        //              <img className="logo" src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="" />
-        //              <a href="https://geekshubsacademy.com/"><img className="logo" src="https://avatars.githubusercontent.com/u/36510045?s=280&v=4" alt="" /></a> 
-        //              <img className="logo" src="https://cdn.icon-icons.com/icons2/2037/PNG/512/facebook_fb_media_social_icon_124262.png" alt="" />
-        //          </div>
-        //          <div className="credit">Created by: Antonio Morales, Jackson Almeida and José Carlos</div>
-        //      </div>
-        //      <div className="footRight">
-        //          <a href="">Privacy Notice</a><br />
-        //          <a href="">Cookies Notice</a><br />
-        //          <a href="">Conditions of Use</a><br />
-        //      </div>
-        // </ModalFooter>
-
         <div className="footer">
             <div className="footLeft">
                  <a href="">Film News</a><br />
@@ -47,27 +25,6 @@ const Footer = props => {
                  <a href="">Conditions of Use</a><br />
              </div>
         </div>
-
-        // <div className="modal-footer footer">
-        //     <div className="footLeft">
-        //         <a href="">Film News</a><br />
-        //         <a href="">Careers</a><br />
-        //         <a href="">Press Lounge</a><br />
-        //     </div>
-        //     <div>
-        //         <div className="logoContainer">
-        //             <img className="logo" src="https://cdn-icons-png.flaticon.com/512/174/174855.png" alt="" />
-        //             <img className="logo" src="https://avatars.githubusercontent.com/u/36510045?s=280&v=4" alt="" />
-        //             <img className="logo" src="https://cdn.icon-icons.com/icons2/2037/PNG/512/facebook_fb_media_social_icon_124262.png" alt="" />
-        //         </div>
-        //         <div className="credit">Created by: Antonio Morales, Jackson Almeida and Jose Carlos</div>
-        //     </div>
-        //     <div className="footRight">
-        //         <a href="">Privacy Notice</a><br />
-        //         <a href="">Cookies Notice</a><br />
-        //         <a href="">Conditions of Use</a><br />
-        //     </div>
-        // </div>
     )
 }
 
